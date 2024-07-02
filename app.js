bcf7baaacd4c71dd390c1934a437851bc1ee1793
Mohamed Amir <83864018+Mohamed22198@users.noreply.github.com>
2024-07-02 21:10:57 +0300

    Performed authenticatoin using Auth0

--- app.js
+++ app.js
@@ -1,57 +1,73 @@
 const express = require("express");
 const session = require("express-session");
-const app = express();
 const path = require("path");
-const port = process.env.PORT || 3333;
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-mongoose
-  .connect("mongodb://localhost:27017/football", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+const { auth, requiresAuth } = require("express-openid-connect");
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 3333;
+
+// Database connection
+mongoose.connect("mongodb://localhost:27017/football", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
   .then(() => {
     console.log("Database Connected...");
   })
   .catch((err) => {
     console.error("Error connecting to database:", err);
   });
-app.use(
-  session({
-    secret: "your_secret_key", // Change this to a secret key for session management
-    resave: false,
-    saveUninitialized: true,
-  })
-);
-const matchRoutes = require("./routes/matchRoutes");
-const Football = require("./routes/Match");
-const loginRoutes = require("./routes/authentication");
+
+// Auth0 configuration
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.AUTH0_CLIENT_SECRET,
+  baseURL: process.env.AUTH0_BASE_URL,
+  clientID: process.env.AUTH0_CLIENT_ID,
+  issuerBaseURL: `https://${process.env.AUTH0_DOMAIN}`,
+};
+
+// Middleware
+app.use(auth(config));
 app.use(bodyParser.json());
-// 3rd party middleware
 app.use(bodyParser.urlencoded({ extended: true })); // Use bodyParser for parsing form data
 app.use(express.static(path.join(__dirname, "public")));
-app.use("/dashboard", Football);
-app.use("/login", loginRoutes);
 
-function isAuthenticated(req, res, next) {
-  if (req.session.isLoggedIn) {
-    // If authenticated, proceed to the next route handler
-    return next();
-  }
-  // If not authenticated, redirect to the login page
-  res.redirect("/login");
-}
+// Session management
+app.use(session({
+  secret: "your_secret_key", // Change this to a secret key for session management
+  resave: false,
+  saveUninitialized: true,
+}));
 
-app.get("/login", (req, res) => {
-  res.sendFile(path.join(__dirname, "./public/login/login.html"));
+const matchRoutes = require("./routes/matchRoutes");
+const Football = require("./routes/Match");
+app.use("/dashboard", requiresAuth(), Football);
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("Welcome to the football app. <a href='/login'>Log in</a> | <a href='/logout'>Log out</a>");
 });
 
-// Request main
-app.get("/dashboard", isAuthenticated, (req, res) => {
+app.get("/dashboard", requiresAuth(), (req, res) => {
   res.sendFile(path.join(__dirname, "./public/dashboard/dashboard.html"));
 });
+
 app.get("/main", (req, res) => {
   res.sendFile(path.join(__dirname, "./public/main/main.html"));
+});
+
+app.get("/profile", requiresAuth(), (req, res) => {
+  res.send(JSON.stringify(req.oidc.user));
+});
+
+// Logout route
+app.get('/logout', (req, res) => {
+  res.oidc.logout({ returnTo: process.env.AUTH0_BASE_URL });
 });
 
 app.listen(port, () => {
